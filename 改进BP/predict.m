e40469eb4922265f73d652e891ec6f662a18de2a
yuhe1984 <38157141+yuhe1984@users.noreply.github.com>
2019-06-05 19:39:24 +0800

    Add files via upload

---
+++ 改进BP/predict.m
@@ -0,0 +1,28 @@
+function z = predict(xa)
+global alg;
+% L = length(xa);
+% xa = [xa,ones(L,1)*-1];
+% total = xa * alg.wb;
+% ya = logsig(total);
+% hidden_input = [ya,-ones(L,1)]; 
+% totall = hidden_input * alg.WB;
+% za = logsig(totall);
+z{1} = xa;
+for i = 2:numel(alg.outputAtLayers)
+    z{i} = logsig(z{i-1} * alg.w{i-1} - alg.t{i-1});
+end
+% total = xa * alg.w{1} - alg.t{1};
+% ya = logsig(total);
+% totall = ya * alg.w{2} - alg.t{2};
+% za = logsig(totall);
+z = z{numel(alg.outputAtLayers)};
+m = size(z,1);
+for i = 1:m
+    if z(i) > 0.5
+        z(i) = 1;
+    elseif z(i) <= 0.5
+        z(i) = 0;
+    end
+end
+%disp(alg.outputAtLayers{3});
+end
